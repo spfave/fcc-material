cee9e17eba80b3b9019de62e48034bea0d0974ab
Sebastian F <sebpfave2@gmail.com>
2021-08-21 23:04:26 -0400

    Create search bar component

--- angular-vgame-db/src/app/app.module.ts
+++ angular-vgame-db/src/app/app.module.ts
@@ -12,9 +12,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchBarComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
