9b2eb98d857ff5a46f3a18ae771a7663942e2cde
Sebastian F <sebpfave2@gmail.com>
2021-08-22 13:47:36 -0400

    Display game details

--- angular-vgame-db/src/app/components/details/details.component.ts
+++ angular-vgame-db/src/app/components/details/details.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent implements OnInit {
-  gameRating: number = 0;
+export class DetailsComponent implements OnInit, OnDestroy {
+  gameRating: number;
+  gameId: string;
+  game: Game;
+  routeSub?: Subscription;
+  gameSub?: Subscription;
 
-  constructor() {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) this.gameSub.unsubscribe();
+    if (this.routeSub) this.routeSub.unsubscribe();
+  }
+
+  getGameDetails(id: string): void {
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((gameResp: Game) => {
+        this.game = gameResp;
+
+        setTimeout(() => {
+          this.gameRating = this.game.metacritic;
+        }, 1000);
+      });
+  }
 
   getColor(value: number): string {
     if (value > 75) {
